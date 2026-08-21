97c9ad539a118644ed08eec58b0c5403cdcb3af0
atticus29 <mark.aaron.fisher@gmail.com>
2026-08-21 17:03:20 -0600

    add data snapshot and add a generalized counter for everything at a given date

--- sum_count_total_aug8.R
+++ sum_count_total_aug8.R
@@ -3,20 +3,31 @@
 csv_path <- "/Users/markfisher/Sites/pcc_cr_mothbox_analyses/2026TaxonomicCompositionDataProcessing_aug_21_snapshot.csv"
 data <- read.csv(csv_path, stringsAsFactors = FALSE)
 
-get_matches <- function(date) {
+get_early_late_date_matches <- function(date) {
 	data[
 		data$Date == date &
 			(startsWith(data$Time, "00-45") | startsWith(data$Time, "03-45")),
 	]
 }
 
+get_all_date_matches <- function(date) {
+	data[
+		data$Date == date,
+	]
+}
+
 calculate_total <- function(date) {
-    matches <- get_matches(date)
-    totalInsect <- sum(as.numeric(matches$CountTotal), na.rm = TRUE)
-    totalMoth <- sum(as.numeric(matches$CountMoths), na.rm = TRUE)
+    early_late_matches <- get_early_late_date_matches(date)
+    totalInsect <- sum(as.numeric(early_late_matches$CountTotal), na.rm = TRUE)
+    totalMoth <- sum(as.numeric(early_late_matches$CountMoths), na.rm = TRUE)
+
+    all_matches <- get_all_date_matches(date)
+    totalInsectAll <- sum(as.numeric(all_matches$CountTotal), na.rm = TRUE)
+    totalMothAll <- sum(as.numeric(all_matches$CountMoths), na.rm = TRUE)
     print(paste("Total Insect Count for", date, ":", totalInsect))
     print(paste("Total Moth Count for", date, ":", totalMoth))
-    # return(list(totalInsect = totalInsect, totalMoth = totalMoth))
+    print(paste("Total Insect Count (All Times) for", date, ":", totalInsectAll))
+    print(paste("Total Moth Count (All Times) for", date, ":", totalMothAll))
 }
 
 calculate_total("8/8/2026")
